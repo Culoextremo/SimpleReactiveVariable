54db4e333af5bc22cd013cd0bce080b51612ed38
Cutremo <32410584+Culoextremo@users.noreply.github.com>
2020-01-27 21:57:48 +0100

    Add files via upload

---
+++ ReactiveVariable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class ReactiveVariable<T>
+{
+    public event Action<T> onValueChanged;
+    private T value;
+
+    public T Value
+    {
+        get
+        {
+            return this.value;
+        }
+        set
+        {
+            if (!Equals(this.value, value))
+            {
+                this.value = value;
+                onValueChanged?.Invoke(value);
+            }
+        }
+    }
+}
